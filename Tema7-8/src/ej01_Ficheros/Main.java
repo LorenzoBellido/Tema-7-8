eb12f3d0280fcf2516084a9c18799df0bccc9f8c
LorenzoBellido <lorenzo.bellido@iesnervion.es>
2024-05-09 12:45:34 +0200

    Ficheros

---
+++ Tema7-8/src/ej01_Ficheros/Main.java
@@ -0,0 +1,28 @@
+package ej01_Ficheros;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		FileReader read;
+		int suma = 0;
+		try {
+			Scanner sc = new Scanner(new FileReader("src/ej01_Ficheros/NumerosReales.txt"));
+			while(sc.hasNext()) {
+				suma += sc.nextInt();
+				
+			}
+			System.out.println(suma);
+		}catch(FileNotFoundException e) {
+			System.out.println("Archivo no encontrado.");
+		}
+		
+		
+
+	}
+
+}
